f2258c75c4c5b6a1582ab99b077437b43e49a47a
Alef Silva <alefsilvadn@gmail.com>
2023-08-02 19:46:44 -0300

    Update home
    Esqueci de salvar na IDE aslkdjaslkdjalksdjlajsdlasd

--- src/Home.java
+++ src/Home.java
@@ -3,6 +3,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import model.clientF.BankClient;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Home extends JFrame {
 
@@ -42,6 +44,11 @@ public class Home extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bem vindo ");
+		lblNewLabel.setFont(new Font("Arial Black", Font.PLAIN, 20));
+		lblNewLabel.setBounds(179, 11, 121, 29);
+		contentPane.add(lblNewLabel);
 		
 		setResizable(false);
 		setLocationRelativeTo(null);
